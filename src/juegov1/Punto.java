e4387bf6c215a3d9f76cda0a4b79cb066df472a7
GerryMG <gerardo.andresz4@live.com>
2018-06-24 21:07:52 -0600

    agregando imagen y punto como clases

---
+++ src/juegov1/Punto.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegov1;
+
+/**
+ *
+ * @author gerar
+ */
+public class Punto {
+    public float x,y;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+}
