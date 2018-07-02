acfcfca979e6012f13da885595d934deecaab498
ShulkMaster <shulkmaster@gmail.com>
2018-07-02 15:54:33 -0600

    agregando fondo en el lv1

--- src/juegov1/nvl1.java
+++ src/juegov1/nvl1.java
@@ -12,6 +12,9 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.Image;
+import elements.levelcomponents.ScrollingBackground;
+import elements.gameactor.Player;
+import org.newdawn.slick.geom.Rectangle;
 
 /**
  *
@@ -23,6 +26,9 @@ public class nvl1 extends BasicGameState {
     private ImagenP ugan;
     private Image flip;
     private Character jugador;
+    private ScrollingBackground fondo;
+    private Player jugador2;
+    private Rectangle boundry;
 
     @Override
     public int getID() {
@@ -36,13 +42,19 @@ public class nvl1 extends BasicGameState {
         events = gc.getInput();
         jugador = new Character(10, 500, 2, 0, 10.0f);
         jugador.IniAnimations(flip);
+        fondo = new ScrollingBackground("res/Img/Backgorund/back.jpg", 7, gc);
+        fondo.setDragSpeed(15);
+        jugador2 = new Player("res/Img/Character/assets/player/player.png");
+        boundry = new Rectangle(190, 30, (gc.getWidth() - 700), (gc.getHeight() - 60));
+        jugador2.setBoundries(boundry);
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        fondo.render(gc, grphcs);
         ugan.draw(ugan.position.getX(), ugan.position.getY(), ugan.getEscala());
         jugador.RenderDraw();
-
+        jugador2.render();
     }
 
     @Override
@@ -52,6 +64,8 @@ public class nvl1 extends BasicGameState {
         jugador.actionClick(events);
         jugador.ActionMove(i);
         ugan.actionClick(events);
+        jugador2.update(gc, i);
+        fondo.update(jugador2);
     }
 
 }
