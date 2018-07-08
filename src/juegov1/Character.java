9d1c0869b625caab5f7ac107098ed8dcf1e36e71
GerryMG <gerardo.andresz4@live.com>
2018-07-07 21:56:04 -0600

    plataformas y shapes mierdas

--- src/juegov1/Character.java
+++ src/juegov1/Character.java
@@ -38,6 +38,11 @@ public class Character {
     private SpriteSheet subImage;
     private float velocidadsalto;
     public Shape shape;
+    public Shape upshape;
+    public Shape downshape;
+    public Shape leftshape;
+    public Shape rigthshape;
+
     public boolean puedoSaltar = false;
     private final int desfasey;
     private final int desfaseextra;
@@ -57,6 +62,10 @@ public class Character {
         animations = new Animation[3];
         animations[0] = tijeras.makeAnimation(sprite, 0, 50, 10, 270, 6, 1);
         shape = new Rectangle(this.position.x, this.position.y, this.getAncho(), this.getAlto() - desfaseextra);
+        upshape = new Rectangle(this.position.x, this.position.y - 10, this.getAncho(), 10);
+        downshape = new Rectangle(this.position.x, this.position.y + this.getAlto(), this.getAncho(), 10);
+        rigthshape = new Rectangle(this.position.x + this.getAncho(), this.position.y, 10, this.getAlto());
+        leftshape = new Rectangle(this.position.x - 10, this.position.y, 10, this.getAlto());
 
     }
 
@@ -134,6 +143,10 @@ public class Character {
     public void RenderDraw(Graphics g) {
         this.PrincipalAnimation.draw(position.x, position.y, this.getAncho(), this.getAlto());
         g.draw(this.shape);
+        g.draw(this.downshape);
+        g.draw(this.leftshape);
+        g.draw(this.rigthshape);
+        g.draw(this.upshape);
     }
 
     public void updatePosition(float delta) {
@@ -152,6 +165,10 @@ public class Character {
                         + (float) (0.5f * (gravity) * (float) (Math.pow(tiempo, 2)));
                 this.vey0 = this.vey0 + (this.gravity * tiempo);
                 this.shape.setY(this.position.y);
+                this.downshape.setY(this.position.y);
+                this.leftshape.setY(this.position.y);
+                this.rigthshape.setY(this.position.y);
+                this.upshape.setY(this.position.y);
             }
         } else {
             this.position.y = (float) this.position.y
@@ -159,6 +176,10 @@ public class Character {
                     + (float) (0.5f * (gravity) * (float) (Math.pow(tiempo, 2)));
             this.vey0 = this.vey0 + (this.gravity * tiempo);
             this.shape.setY(this.position.y);
+            this.downshape.setY(this.position.y);
+            this.leftshape.setY(this.position.y);
+            this.rigthshape.setY(this.position.y);
+            this.upshape.setY(this.position.y);
         }
         ActionMove(tiempo);
 
@@ -222,6 +243,10 @@ public class Character {
                         + (float) (0.5f * (gravity) * (float) (Math.pow(tiempo, 2)));
                 this.vey0 = this.vey0 + (this.gravity * tiempo);
                 this.shape.setY(this.position.y);
+                this.downshape.setY(this.position.y + this.getAlto());
+                this.leftshape.setY(this.position.y);
+                this.rigthshape.setY(this.position.y);
+                this.upshape.setY(this.position.y - 10);
             }
         } else {
             this.position.y = (float) this.position.y
@@ -229,112 +254,135 @@ public class Character {
                     + (float) (0.5f * (gravity) * (float) (Math.pow(tiempo, 2)));
             this.vey0 = this.vey0 + (this.gravity * tiempo);
             this.shape.setY(this.position.y);
+
+            this.downshape.setY(this.position.y + this.getAlto());
+            this.leftshape.setY(this.position.y);
+            this.rigthshape.setY(this.position.y);
+            this.upshape.setY(this.position.y - 10);
             System.out.println(this.Choca(con.lista));
-            if (this.Choca(con.lista) == UtilEnum.Y) {
-                if (this.vey0 > 0) {
-                    this.vey0 = -(this.vey0 * 2);
-                } else {
-                    this.vey0 = 1;
-                }
+            if (this.Choca(con.lista) == UtilEnum.YU) {
+                this.vey0 = 2;
                 this.aceleracionx = 0;
                 this.position = this.LastPosition;
                 this.puedoSaltar = true;
                 this.shape.setY(this.position.y);
+                this.downshape.setY(this.position.y + this.getAlto());
+                this.leftshape.setY(this.position.y);
+                this.rigthshape.setY(this.position.y);
+                this.upshape.setY(this.position.y - 10);
+            } else {
+                if (this.Choca(con.lista) == UtilEnum.YD) {
+                    this.position = this.LastPosition;
+                    this.downshape.setY(this.position.y + this.getAlto());
+                    this.leftshape.setY(this.position.y);
+                    this.rigthshape.setY(this.position.y);
+                    this.upshape.setY(this.position.y - 10);
+                    this.vey0 = -250;
+                }
             }
         }
         ActionMove(tiempo, con);
 
-    }
-
-    public void update(int delta, ContainerS con) {
-        float tiempo = (float) (delta / 1000);
-        this.LastPosition = this.position;
-        float y0 = this.position.y;
-        if (y0 + (this.PrincipalAnimation.getHeight() * this.getEscala()) - (this.getDesfase() * this.getEscala()) >= JuegoV1.contenedor.getHeight()) {
-            this.position.y = JuegoV1.contenedor.getHeight() - (this.PrincipalAnimation.getHeight() * this.getEscala()) + (this.getDesfase() * this.getEscala());
-            //System.out.println(this.position.y);
-            this.shape.setY(this.position.y);
-            //System.out.println(JuegoV1.contenedor.getHeight() - (this.PrincipalAnimation.getHeight() * escala) + (desfase * escala));
-            if (this.getVey0() == this.velocidadsalto) {
-                //System.out.println("salto");
-                this.position.y = (float) this.position.y
-                        - (float) (this.getVey0() * tiempo)
-                        + (float) (0.5f * (gravity) * (float) (Math.pow(tiempo, 2)));
-                this.setVey0(this.getVey0() + (this.gravity * tiempo));
-                this.shape.setY(this.position.y);
-            }
-        } else {
-            this.position.y = (float) this.position.y
-                    - (float) (this.getVey0() * tiempo)
-                    + (float) (0.5f * (gravity) * (float) (Math.pow(tiempo, 2)));
-            this.setVey0(this.getVey0() + (this.gravity * tiempo));
-            this.shape.setY(this.position.y);
-            if (this.Choca(con.lista) == UtilEnum.Y) {
-                //this.position = this.LastPosition;
-                this.puedoSaltar = true;
-                this.shape.setY(this.position.y);
-            }
-
-        }
-        ActionMove(tiempo, con);
     }
 
     private void ActionMove(float tiempo, ContainerS con) {
         if (JuegoV1.contenedor.getWidth() < this.position.x + this.getAncho()) {
             this.position.x = JuegoV1.contenedor.getWidth() - this.getAncho();
             this.shape.setX(this.position.x);
+            this.shape.setX(this.position.x);
+            this.downshape.setX(this.position.x);
+            this.leftshape.setX(this.position.x - 10);
+            this.rigthshape.setX(this.position.x + this.getAncho());
+            this.upshape.setX(this.position.x);
+
             this.vx = 0;
         } else {
             if (this.position.x < 0) {
                 this.position.x = 0;
                 this.shape.setX(this.position.x);
+                this.downshape.setX(this.position.x);
+                this.leftshape.setX(this.position.x - 10);
+                this.rigthshape.setX(this.position.x + this.getAncho());
+                this.upshape.setX(this.position.x);
                 this.vx = 0;
                 this.puedoSaltar = false;
 
-            }
-        }
-
-        this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-        this.shape.setX(this.position.x);
-        System.out.println(this.Choca(con.lista));
-        if (this.Choca(con.lista) == UtilEnum.Y) {
-            this.position = this.LastPosition;
-            if (this.vx > 0) {
-                this.vx = -250;
-                //this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-               // this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-                // this.aceleracionx = -500;
             } else {
-                if (this.vx < 0) {
-                    this.vx = 250;
-                   // this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-                    //this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-                    //this.aceleracionx = 500;
+                this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                this.shape.setX(this.position.x);
+                this.downshape.setX(this.position.x);
+                this.leftshape.setX(this.position.x - 10);
+                this.rigthshape.setX(this.position.x + this.getAncho());
+                this.upshape.setX(this.position.x);
+
+                System.out.println(this.Choca(con.lista));
+                if (this.Choca(con.lista) == UtilEnum.XR) {
+                    this.position = this.LastPosition;
+                    /*if (this.vx > 0) {
+                        this.vx = -250;
+                        //this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                        // this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                        // this.aceleracionx = -500;
+                    } else {
+                        if (this.vx < 0) {
+                            this.vx = 250;
+                            // this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                            //this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                            //this.aceleracionx = 500;
+                        }
+                    }*/
+                } else {
+                    if (this.Choca(con.lista) == UtilEnum.XL) {
+                        this.position = this.LastPosition;
+                    }
                 }
+                /*this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                //this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
+                // this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));*/
+                this.shape.setX(this.position.x);
+                this.downshape.setX(this.position.x);
+                this.leftshape.setX(this.position.x - 10);
+                this.rigthshape.setX(this.position.x + this.getAncho());
+                this.upshape.setX(this.position.x);
             }
         }
-        this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-        //this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-        // this.position.x = this.position.x + (this.vx * (tiempo)) + (0.5f * aceleracionx * (float) (Math.pow(tiempo, 2)));
-        this.shape.setX(this.position.x);
-        //aca
 
+        //aca
     }
 
     public UtilEnum Choca(Platform[] recList) {
         UtilEnum response = UtilEnum.N;
         for (Platform r : recList) {
-            if (this.shape.intersects(r.getContenedor())) {
+            if (this.downshape.intersects(r.getContenedor())) {
+
+                // this.position.y = r.getContenedor().getMaxY() - this.getAlto();
                 /*
                 if (this.position.y == this.LastPosition.y) {
                     response = UtilEnum.X;
                 }else {
                     response = UtilEnum.Y;
                 }*/
-                
-                response = UtilEnum.Y;
+                response = UtilEnum.YU;
 
+            } else {
+                if (this.upshape.intersects(r.getContenedor())) {
+                    this.vey0 = 0;
+                    //this.position.y = r.getContenedor().getMinY();
+                    response = UtilEnum.YD;
+                } else {
+                    if (this.rigthshape.intersects(r.getContenedor())) {
+                        // this.position.x = r.getContenedor().getMinX() - this.getAncho() - 1;
+                        response = UtilEnum.XL;
+                    } else {
+                        if (this.leftshape.intersects(r.getContenedor())) {
+                            //this.position.x = r.getContenedor().getMaxX();
+                            response = UtilEnum.XR;
+                        }
+                    }
+
+                }
             }
+
         }
 
         return response;
